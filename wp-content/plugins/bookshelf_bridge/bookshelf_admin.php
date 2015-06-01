dec6f9c8d3040a60a19e50729ef46fd700fdcd79
Tachi <zaveruha007@gmail.com>
2015-06-01 14:14:41 +0300

    Bridge PLugin

--- wp-content/plugins/bookshelf_bridge/bookshelf_admin.php
+++ wp-content/plugins/bookshelf_bridge/bookshelf_admin.php
@@ -1,2 +1,6 @@
-<?php
-echo 'Admin File';
+<div class="wrap bookshelf_admin">
+	<?php echo "<h2>".__('Bookshelf Product Display Options','bookshelf_trdom')."</h2>";?>
+	<form name="bookshelf_from" method="post" action="">
+		
+	</form>
+</div>
