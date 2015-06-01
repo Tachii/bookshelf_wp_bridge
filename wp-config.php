83364e41043eda1099f0cd5626d07c2fa3ff6500
Tachi <zaveruha007@gmail.com>
2015-06-01 12:41:23 +0300

    Initial WP Installation

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'heroku_a7073983d067b85');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'b434f243f31da6');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', '5b370255');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'us-cdbr-iron-east-02.cleardb.net');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
